c855f3b1209eacf2bdc99051479cc0d02b60325b
Sergey Ilin <ilin_sergey90@mail.ru>
2024-02-05 21:27:40 +0300

    c done

--- django_views_routing_homework/views/level_1/c_baned_username.py
+++ django_views_routing_homework/views/level_1/c_baned_username.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
-
+from django.http import HttpRequest, HttpResponse
 
 """
 В этой вьюхе мы хотим проверять забанен ли юзернэйм или нет.
@@ -7,12 +6,13 @@ from django.http import HttpResponse
 Задания:
     1. Если юзернэйм в списке забаненных BANNED_USERNAMES, то возрващайте сообщение: User banned,
        иначе возвращайте сообщение: User not banned
-    2. Результат проверяйте по ссылке http://127.0.0.1:8000/banned/тут интересующий юзернэйм/, 
+    2. Результат проверяйте по ссылке http://127.0.0.1:8000/banned/тут интересующий юзернэйм/,
        например http://127.0.0.1:8000/banned/any_username/
 """
 BANNED_USERNAMES = ['red_dev', 'green_bear', 'monster']
 
 
-def is_username_banned_view(request, username: str):
-    # код писать тут
+def is_username_banned_view(request: HttpRequest, username: str) -> HttpResponse:
+    if username in BANNED_USERNAMES:
+        return HttpResponse('User banned')
     return HttpResponse('User not banned')
